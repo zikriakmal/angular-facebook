edd83119ac0d9df4b29b97a913000c411f8d7008
Zikri Akmal S <zikriakmale@gmail.com>
2024-10-12 21:13:59 +0700

    feat: using click in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginPageComponent },
 ];
