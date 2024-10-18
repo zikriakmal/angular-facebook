4d520763b66038be9d3045fe9c6006537d5337da
Zikri Akmal S <zikriakmale@gmail.com>
2024-10-18 21:27:13 +0700

    feat: home top sticky nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
+    { path: 'home', component: HomePageComponent },
     { path: 'login', component: LoginPageComponent },
     { path: 'register', component: RegisterPageComponent },
 ];
